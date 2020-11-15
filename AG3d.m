fb1390b6f5cc6b37b54e71c23b3b3b424190358d
mauriciosanchezmusic <61812923+mauriciosanchezmusic@users.noreply.github.com>
2020-11-15 14:44:34 -0600

    Add files via upload

---
+++ AG3d.m
@@ -0,0 +1,148 @@
+%AG 3d
+clear,clc;
+fid = 8;
+numIndiv=5;
+LI = [0,0];
+LS = [5,6];
+GenMAX = 400;
+dim=2;
+AlgGen(fid,numIndiv,LI,LS,GenMAX);
+%%
+function minimo = AlgGen(fid,numIndiv,LI,LS,GenMAX)
+p = GenerarPoblacion(numIndiv,LI,LS);
+cosPob = Evaluar(fid,p,numIndiv);
+[minimo, im] = min(cosPob);
+Sact = p(:,im);
+Gen = 1;
+f1= figure('Name','Sol X','NumberTitle','off');
+h = animatedline;
+f2= figure('Name','Sol Y','NumberTitle','off');
+h2 = animatedline;
+Impresion3d(Gen,Sact,h,h2);
+while Gen<GenMAX 
+    S = Seleccion(numIndiv,p,fid);
+    C = Criadero(S,p,numIndiv,LI,LS);
+    p = comparar(p,C,fid);
+    p = mutacion(p,numIndiv,LI,LS);
+    Gen = Gen+1;
+    [minimo, im] = min(cosPob);
+    Sact = p(:,im);
+    historico(:,Gen)=Sact;
+    Impresion3d(Gen,Sact,h,h2)
+    if (Gen>50 & (historico(:,Gen)==historico(:,Gen-25)))
+        break;
+    end
+end
+minimo = Sact
+fprintf("Coste: %0.4f\n",bfm(fid,Sact));
+end
+
+%%
+function c = Evaluar(fid,p,numind)
+    if numind==1
+        c(1) = bfm(fid,p);  
+    else
+        for i = 1:numind
+            pob=[p(1,i),p(2,i)];
+            c(i) = bfm(fid,pob);  
+        end
+    end
+end
+%%
+function s = Seleccion(numIndiv,pob,fid)
+%Torneo
+n = 2;
+MatrizConcursantes = zeros(n,2);
+MatrizDescendientes = zeros(numIndiv,2);
+
+    while (sum(MatrizDescendientes(:,1))<numIndiv)
+        i = 1;
+        while (i<=n)
+            a = randi([1,numIndiv],1,1);
+            if a~= MatrizConcursantes(:,2)
+                MatrizConcursantes (i,:) = [Evaluar(fid,[pob(1,a),pob(2,a)],1),a];
+                i=i+1;
+            end
+        end
+        [r o]=min(MatrizConcursantes(:,1));
+        MatrizDescendientes(MatrizConcursantes(o,2)) = MatrizDescendientes(MatrizConcursantes(o,2))+1;
+    end
+    s = MatrizDescendientes;
+end
+%%
+function crias = Criadero(S,p,numIndiv,LI,LS)
+    contador =1;
+    for i = 1:numIndiv
+        if(S(i,1)~=0)
+            for j = 1:S(i,1)
+                [crias(1,contador),crias(2,contador)] = Engendrar(p(:,i),LI,LS);
+                contador=contador+1;
+            end
+        end
+    end
+
+end
+%%
+function [engendrox,engendroy] = Engendrar(padre,LI,LS)
+    engendrox = padre(1)+(padre(1)* (-0.009 + (.009+.009).*rand()));
+    engendroy = padre(2)+(padre(2)* (-0.009 + (.009+.009).*rand()));
+    while (engendrox<LI(1) || engendrox>LS(1))
+        engendrox = padre(1)+(padre(1)* (-0.009 + (.009+.009).*rand()));
+    end
+    while (engendroy<LI(2) || engendroy>LS(2))
+        engendroy = padre(2)+(padre(2)* (-0.009 + (.009+.009).*rand()));
+    end
+end
+%%
+function pnew = comparar(p,c,fid)
+    for i=1:(numel(p)/2)
+        if(bfm(fid,c(:,i))<bfm(fid,p(:,i)))
+            pnew(:,i)=c(:,i);
+        else
+            pnew(:,i) = p(:,i);
+        end
+    end
+end
+%%
+function pob = mutacion(pob,numIndiv,LI,LS)
+pm = 0.02;
+    for i=1:numIndiv
+        if(rand()<  pm)
+           tempx = pob(1,i);
+           tempy = pob(2,i);
+           pob(1,i) = tempx+tempx*0.005*randi([-1,1],1,1); 
+           pob(2,i) = tempy+tempy*0.005*randi([-1,1],1,1); 
+           while (pob(1,i)<LI(1) || pob(1,i)>LS(1))
+                pob(1,i) = tempx+tempx*0.005*randi([-1,1],1,1); 
+           end
+           while (pob(2,i)<LI(2) || pob(2,i)>LS(2))
+                pob(2,i) = tempy+tempy*0.005*randi([-1,1],1,1); 
+           end
+        end
+    end 
+end
+%%
+function pob = GenerarPoblacion(numIndiv,LI,LS)
+    x(1) = (LI(1) + (LS(1)-LI(1))*rand());
+    y(1) = (LI(2) + (LS(2)-LI(2))*rand());
+    pob(1,1)=x;
+    pob(2,1)=y;
+    for i = 2:numIndiv
+         x(i) =  x(i-1)-rand()*randi([-1,1],1,1);
+         y(i) =  y(i-1)-rand()*randi([-1,1],1,1);
+         while (x(i)<min(LI(1)) || x(i)>max(LS(1)))
+            x(i) =  x(i-1)-rand()*randi([-1,1],1,1);  
+         end
+         while (y(i)<min(LI(2)) || y(i)>max(LS(2)))
+            y(i) =  y(i-1)-rand()*randi([-1,1],1,1);  
+         end 
+         pob(1,i)=x(i);
+         pob(2,i)=y(i);
+    end
+end
+%%
+function Impresion3d(cont,Sact,h,h2)
+    addpoints(h,cont,Sact(1,1));
+    addpoints(h2,cont,Sact(2,1));
+    drawnow;
+end
---
+++ SA.m
@@ -0,0 +1,101 @@
+fid = 12;
+% LimI = [x , y ];
+% LimS = [x , y] ;
+LimI = [-5,-5];
+LimS = [5,5];
+NR=7;
+Ti=15;
+dim=2;
+CalculaSA1(fid,LimI,LimS,NR,Ti,dim);
+%%
+function Min = CalculaSA1(fID,limiteInf,limiteSup, Nrepe,T,dim)
+    Tf = 0.0000001;
+  % Imprmimir Funcion Objetivo***********************************************
+    %fplot(bfm(fID,~),[-10,10],'k'),hold on;
+  % Primeras Soluciones *****************************************************
+  if(dim==1)
+    Sact = (limiteInf+limiteSup)/2;
+  else
+    Sact = [mean(limiteInf) mean(limiteSup)];
+  end
+  % Modelo Metropolis********************************************************
+    CritAcep = @(T, delta) exp(delta/T);
+  % Ciclo de busqueda********************************************************
+    cont=1;
+    if(dim==1)
+        f1 = figure;
+        h = animatedline;
+        Impresion2d(cont,Sact,h);
+    else
+        f1= figure('Name','Sol X','NumberTitle','off');
+        h = animatedline;
+        f2= figure('Name','Sol Y','NumberTitle','off');
+        h2 = animatedline;
+        Impresion3d(cont,Sact,h,h2);
+    end
+    while(T>Tf)
+       for c = 1:Nrepe
+           if(dim==1)
+            Scand = GenerarVecino(Sact,limiteInf,limiteSup);
+           else
+            Scand = GenerarVecino3d(Sact,limiteInf,limiteSup);
+           end
+            delta = bfm(fID,Scand) - bfm(fID,Sact);
+            if(rand()> CritAcep(T,delta) || delta<0)
+               Sact = Scand; 
+            end
+            if(dim==1)
+                Impresion2d(cont,Sact,h);
+                cont=cont+1;
+            else
+                Impresion3d(cont,Sact,h,h2);
+                cont=cont+1;
+            end
+       end
+       T = Cooling(T);
+    end
+    % Impresion de Resultados**************************************************
+    f2 = figure;
+    plot(Sact,bfm(fID,Sact),'d',"markersize",10);
+    disp("Valor de X= ");
+    disp(Sact);
+    disp("Coste= ");
+    disp(bfm(fID,Sact));
+    Min = Sact;
+ 
+end
+%%
+function x = GenerarVecino(SolActual,limInf,limSup)
+     x = SolActual+1*(-1 + (1+1)*rand());
+     while(x<limInf || x>limSup)
+        x = SolActual+1*(-1 + (1+1)*rand());
+     end
+end
+%%
+function Temp = Cooling(T)
+Temp = 0.85*(T);
+end
+%%
+function Impresion2d(cont,Sact,h)
+        addpoints(h,cont,Sact);
+        cont=cont+1;
+        drawnow;
+end
+%%
+function Impresion3d(cont,Sact,h,h2)
+    addpoints(h,cont,Sact(1,1));
+    addpoints(h2,cont,Sact(1,2));
+    drawnow;
+end
+%%
+ function Snew = GenerarVecino3d(SolActual,limInf,limSup)
+     x = SolActual(1,1)+1*(-1 + (1+1)*rand());
+     y = SolActual(1,2)+1*(-1 + (1+1)*rand());
+     while(x<limInf(1) || x>limSup(1))
+        x = SolActual(1,1)+1*(-1 + (1+1)*rand());
+     end
+     while(y<limInf(2) || y>limSup(2))
+        y = SolActual(1,2)+1*(-1 + (1+1)*rand());
+     end
+     Snew = [x y];
+end
