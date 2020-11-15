49ad805c221a819156994723c2b15c8fd48cc50f
David Hernández <d.hdza19@gmail.com>
2020-11-15 15:28:09 -0600

    Algoritmos

---
+++ Tercera Entrega/AG.m
@@ -0,0 +1,118 @@
+clear,clc;
+fid = 13;
+numIndiv=5;
+LI=[5,5];
+LS=[10,10];
+GenMAX = 500;
+dim=2;
+AlgGen(fid,numIndiv,LI,LS,GenMAX);
+%%
+function minimo = AlgGen(fid,numIndiv,LI,LS,GenMAX)
+p = GenerarPoblacion(numIndiv,LI,LS);
+cosPob = Evaluar(fid,p,numIndiv);
+[minimo, im] = min(cosPob);
+Sact = p(im);
+historico(1) = Sact;
+Gen = 1;
+f1 = figure;
+h=animatedline;
+addpoints(h,1,Sact);
+while Gen<GenMAX 
+    S = Seleccion(numIndiv,p,fid);
+    C = Criadero(S,p,numIndiv,LI,LS);
+    p = comparar(p,C,fid);
+    p = mutacion(p,numIndiv,LI,LS);
+    Gen = Gen+1;
+    [minimo, im] = min(cosPob);
+    Sact = p(im);
+    historico(Gen) = Sact;
+    addpoints(h,Gen,Sact);
+    drawnow;
+    if (Gen>50 && historico(Gen)==historico(Gen-25))
+        break;
+    end
+end
+minimo = Sact
+fprintf("Coste: %0.4f\n",bfm(fid,Sact));
+end
+%%
+function c = Evaluar(fid,Poblacion,numind)
+    for i = 1:numind
+        c(i) = bfm(fid,Poblacion(i));  
+    end
+end
+%%
+function s = Seleccion(numIndiv,pob,fid)
+%Torneo
+n = 2;
+MatrizConcursantes = zeros(n,2);
+MatrizDescendientes = zeros(numIndiv,1);
+
+    while (sum(MatrizDescendientes)<numIndiv)
+        i = 1;
+        while (i<=n)
+            a = randi([1,numIndiv],1,1);
+            if a~= MatrizConcursantes(:,2)
+                MatrizConcursantes (i,:) = [Evaluar(fid,pob(a),1),a];
+                i=i+1;
+            end
+        end
+        [r o]=min(MatrizConcursantes(:,1));
+        MatrizDescendientes(MatrizConcursantes(o,2)) = MatrizDescendientes(MatrizConcursantes(o,2))+1;
+    end
+    s = MatrizDescendientes;
+end
+%%
+function crias = Criadero(S,p,numIndiv,LI,LS)
+    contador =1;
+    for i = 1:numIndiv
+        if(S(i)~=0)
+            for j = 1:S(i)
+                crias(contador) = Engendrar(p(i),LI,LS);
+                contador=contador+1;
+            end
+        end
+    end
+
+end
+%%
+function engendro = Engendrar(padre,LI,LS)
+    engendro = padre+(padre* (-0.001 + (.001+.001).*rand()));
+    while (engendro<LI || engendro>LS)
+        engendro = padre+(padre* (-0.001 + (.001+.001).*rand()));
+    end
+end
+%%
+function pnew = comparar(p,c,fid)
+    for i=1:numel(p)
+        if(bfm(fid,c(i))<bfm(fid,p(i)))
+            pnew(i)=c(i);
+        else
+            pnew(i) = p(i);
+        end
+    end
+end
+%%
+function pob = mutacion(pob,numIndiv,LI,LS)
+pm = 0.02;
+    for i=1:numIndiv
+        if(rand()<  pm)
+           temp = pob(i);
+           pob(i) = temp+temp*0.0005*randi([-1,1],1,1); 
+           while (pob(i)<LI || pob(i)>LS)
+                pob(i) = temp+temp*0.0005*randi([-1,1],1,1); 
+           end
+        end
+    end 
+end
+%%
+%%
+function p = GenerarPoblacion(numIndiv,LI,LS)
+    p(1) = (LI + (LS-LS)*rand());
+    for i = 2:numIndiv
+         p(i) =  p(i-1)-rand()*randi([-1,1],1,1);  
+         while (p(i)<LI || p(i)>LS)
+         p(i) =  p(i-1)-rand()*randi([-1,1],1,1);  
+         end
+    end
+end
