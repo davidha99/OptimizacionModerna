fdd0aeff5d4000a1b2e84ff2f72c59e9aa6c0d9f
David Hernández <d.hdza19@gmail.com>
2020-11-21 23:27:58 -0600

    Modified verificarRangoXY

--- verificarRangoXY.m
+++ verificarRangoXY.m
@@ -3,9 +3,13 @@ function sw2 = verificarRangoXY(rango, limInfX, limSupX, limInfY, limSupY)
     if isrow(rango)
         if limInfX > limSupX || limInfY > limSupY
             sw2 = false;
-        elseif ~(rango(1) <= limInfX && limInfX <= rango(2)) || ~(rango(1) <= limSupX && limSupX <= rango(2))
+        elseif ~(rango(1) <= limInfX && limInfX <= rango(2))
             sw2 = false;
-        elseif ~(rango(1) <= limInfY && limInfY <= rango(2)) || ~(rango(1) <= limSupY && limSupY <= rango(2))
+        elseif ~(rango(1) <= limSupX && limSupX <= rango(2))
+            sw2 = false;
+        elseif ~(rango(1) <= limInfY && limInfY <= rango(2))
+            sw2 = false;
+        elseif ~(rango(1) <= limSupY && limSupY <= rango(2))
             sw2 = false;
         end
     else
@@ -13,9 +17,13 @@ function sw2 = verificarRangoXY(rango, limInfX, limSupX, limInfY, limSupY)
         rangoY = [rango(2,1) rango(2,2)];
         if limInfX > limSupX || limInfY > limSupY
             sw2 = false;
-        elseif ~(rangoX(1) <= limInfX && limInfX <= rangoX(2)) || ~(rangoX(1) <= limSupX && limSupX <= rangoX(2))
+        elseif ~(rangoX(1) <= limInfX && limInfX <= rangoX(2))
             sw2 = false;
-        elseif ~(rangoY(1) <= limInfY && limInfY <= rangoY(2)) || ~(rangoY(1) <= limSupY && limSupY <= rangoY(2))
+        elseif ~(rangoX(1) <= limSupX && limSupX <= rangoX(2))
+            sw2 = false;
+        elseif ~(rangoY(1) <= limInfY && limInfY <= rangoY(2))
+            sw2 = false;
+        elseif ~(rangoY(1) <= limSupY && limSupY <= rangoY(2))
             sw2 = false;
         end
     end
